49e854d1d5b13fb8f89c90387916dcdf99112fbb
Muraoka Masaki <muraokamasaki96@gmail.com>
2020-03-22 22:40:30 +0900

    Added sqlite support for documents.

--- document.go
+++ document.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/csv"
+	_ "github.com/mattn/go-sqlite3"
 	"io"
 	"log"
 	"os"
@@ -51,7 +53,7 @@ func wordCount(document string) int {
 // Defines functions that consumes documents.
 type documentFn func(document Document)
 
-// Reads through a CSV file of columns 'id', 'Title', 'Body' and 'URL' and applies a function to each document.
+// Reads through a CSV file of columns 'id', 'title', 'body' and 'URL' and applies a function to each document.
 func readDocumentFromCSV(filename string, fn documentFn) {
 	f, err := os.Open(filename)
 	if err != nil {
@@ -82,7 +84,7 @@ func readDocumentFromCSV(filename string, fn documentFn) {
 	}
 }
 
-// Retrieve a list of documents from a CSV with columns 'id', 'Title', 'Body' and 'URL.
+// Retrieve a list of documents from a CSV with columns 'id', 'title', 'body' and 'URL.
 func getDocumentFromCSV(filename string, ids []int) (resultsList []Document) {
 	resultsList = make([]Document, len(ids))
 
@@ -126,6 +128,46 @@ func getDocumentFromCSV(filename string, ids []int) (resultsList []Document) {
 			}
 			idPointer++
 		}
+	}
+	return
+}
+
+// Read from a SQL database table 'documents' with attributes 'id', 'title', 'body' and 'URL'
+func readDocumentFromSQL(filename string, fn documentFn) {
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, title, body, URL FROM documents")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	var document Document
+	for rows.Next() {
+		if err = rows.Scan(&document.id, &document.Title, &document.Body, &document.URL); err != nil {
+			log.Fatal(err)
+		}
+		fn(document)
+	}
+}
+
+// Retrieve a list of documents from a SQL database table 'documents' with attributes 'id', 'title', 'body' and 'URL'
+func getDocumentFromSQL(filename string, ids []int) (resultsList []Document) {
+	resultsList = make([]Document, len(ids))
+
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	for idx, id := range ids {
+		row := db.QueryRow("SELECT id, title, body, URL FROM documents WHERE id=?", id)
+		row.Scan(&resultsList[idx].id, &resultsList[idx].Title, &resultsList[idx].Body, &resultsList[idx].URL) // Ignore error, skip idx if no document can be found.
 	}
 	return
 }
--- document_test.go
+++ document_test.go
@@ -55,6 +55,15 @@ func TestDocumentList_AverageDocumentLength(t *testing.T) {
 	}
 }
 
+func TestReadDocumentFromCSV(t *testing.T) {
+	wanted := []string{"Cohen's kappa", "Latent semantic analysis", "Code-division multiple access"}
+	readDocumentFromCSV("example.csv", func (d Document) {
+		 if wanted[d.id - 1] != d.Title {
+			 t.Errorf("Read wrong document. Got %v, Wanted %v.", d.Title, wanted[d.id - 1])
+		 }
+	})
+}
+
 func TestGetDocumentFromCSV(t *testing.T) {
 	resultList := getDocumentFromCSV("example.csv", []int{2, 1, 3})
 	wanted := []string{"Latent semantic analysis", "Cohen's kappa", "Code-division multiple access"}
@@ -63,6 +72,30 @@ func TestGetDocumentFromCSV(t *testing.T) {
 	}
 	// Test getting non-existent document.
 	resultList = getDocumentFromCSV("example.csv", []int{4})
+	for _, result := range resultList {
+		if result.id != 0 && result.Title != "" && result.Body != "" && result.URL != "" {
+			t.Errorf("Retrived wrong document. Got %v, Wanted empty document", result)
+		}
+	}
+}
+
+func TestReadDocumentFromSQL(t *testing.T) {
+	wanted := []string{"Cohen's kappa", "Latent semantic analysis", "Code-division multiple access"}
+	readDocumentFromSQL("example.db", func (d Document) {
+		if wanted[d.id - 1] != d.Title {
+			t.Errorf("Read wrong document. Got %v, Wanted %v.", d.Title, wanted[d.id - 1])
+		}
+	})
+}
+
+func TestGetDocumentFromSQL(t *testing.T) {
+	resultList := getDocumentFromSQL("example.db", []int{2, 1, 3})
+	wanted := []string{"Latent semantic analysis", "Cohen's kappa", "Code-division multiple access"}
+	if resultList[0].Title != wanted[0] && resultList[1].Title != wanted[1] && resultList[2].Title != wanted[2] {
+		t.Errorf("Wrong document titles. Got %v, Wanted %v.", resultList, wanted)
+	}
+	// Test getting non-existent document.
+	resultList = getDocumentFromSQL("example.db", []int{4})
 	for _, result := range resultList {
 		if result.id != 0 && result.Title != "" && result.Body != "" && result.URL != "" {
 			t.Errorf("Retrived wrong document. Got %v, Wanted empty document", result)
